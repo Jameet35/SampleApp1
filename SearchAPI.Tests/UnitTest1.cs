ca06fdaa5e1623a888c8b24d80295e5da8259efe
Jameet35 <Jameet.Singh@cognizant.com>
2024-07-03 12:51:06 +0530

    Update UnitTest1.cs

--- SearchAPI.Tests/UnitTest1.cs
+++ SearchAPI.Tests/UnitTest1.cs
@@ -5,6 +5,11 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        Assert.True(1==1);
+    }
+    [Fact]
+    public void Test1()
+    {
         Assert.True(2==1);
     }
 }
